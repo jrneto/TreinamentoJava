8e3df777c2a9ffb20be7a4c6dc1cff3901294e26
CIT\jreato <jreato@ciandt.com>
2019-03-02 22:37:43 -0300

    treinamento java

--- src/com/ciandt/training/challenge1/CaixaEletronico.java
+++ src/com/ciandt/training/challenge1/CaixaEletronico.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 public class CaixaEletronico {
 	 
-	private Map<Notas, Integer> caixa = new HashMap<Notas, Integer>();
-	private Map<Notas, Integer> notas = new HashMap<Notas, Integer>();
+	private Map<Cedula, Integer> caixa = new TreeMap<Cedula, Integer>();
+	private Map<Cedula, Integer> cedulasSacadas = new TreeMap<Cedula, Integer>();
 	private double valorSaque;
 	
 	private final double VALOR_MAXIMO = 10000d;
@@ -19,62 +20,63 @@ public class CaixaEletronico {
 	}
 	
 	private void iniciar() {
-		caixa.put(Notas.DOIS, QTD_NOTAS);
-		caixa.put(Notas.CINCO, QTD_NOTAS);
-		caixa.put(Notas.DEZ, QTD_NOTAS);
-		caixa.put(Notas.VINTE, QTD_NOTAS);
-		caixa.put(Notas.CINQUENTA, QTD_NOTAS);
-		caixa.put(Notas.CEM, QTD_NOTAS);
+		caixa.put(Cedula.DOIS, QTD_NOTAS);
+		caixa.put(Cedula.CINCO, QTD_NOTAS);
+		caixa.put(Cedula.DEZ, QTD_NOTAS);
+		caixa.put(Cedula.VINTE, QTD_NOTAS);
+		caixa.put(Cedula.CINQUENTA, QTD_NOTAS);
+		caixa.put(Cedula.CEM, QTD_NOTAS);
 	}
 	
-	private void zerarNotas() {
-		notas.put(Notas.DOIS, 0);
-		notas.put(Notas.CINCO, 0);
-		notas.put(Notas.DEZ, 0);
-		notas.put(Notas.VINTE, 0);
-		notas.put(Notas.CINQUENTA, 0);
-		notas.put(Notas.CEM, 0); 
+	private void zerarCedulasSacadas() {
+		cedulasSacadas.put(Cedula.DOIS, 0);
+		cedulasSacadas.put(Cedula.CINCO, 0);
+		cedulasSacadas.put(Cedula.DEZ, 0);
+		cedulasSacadas.put(Cedula.VINTE, 0);
+		cedulasSacadas.put(Cedula.CINQUENTA, 0);
+		cedulasSacadas.put(Cedula.CEM, 0); 
 	}
 
 	public void sacar(int valor) { 
 		System.out.println("##### Início Saque #####");
 		System.out.println("Valor saque: " + valor);
 		visualizarTotalCaixa();
-		zerarNotas();
+		zerarCedulasSacadas();
 		this.valorSaque = valor;
 		
 		if (!valorValido(valor)) {
 			return;
 		} 
 		 
-		obterNotas(valor);
+		obterCedulas(valor);
 		
-		visualizarNotasSacadas();
+		visualizarCedulasSacadas();
 		System.out.format("Saque de %s efetuado!%n",valorSaque);
 		System.out.println("Obrigado, volte sempre!");
 		System.out.println("##### Fim Saque #####");
 		
 	}
 
-	private void obterNotas(int valor) {  
-		int qtdNotas; 		
-		Notas maiorNota = Notas.getNota(obterMaiorNotaDisponivel(valor));
+	private void obterCedulas(int valor) {  
+		int qtdCedula; 		
+		Cedula maiorCedula = Cedula.getCedula(obterCedulaDisponivel(valor));
 		while (valor > 0) {
-			qtdNotas = 0;
-			if (valor >= maiorNota.valorNota ) {
-				qtdNotas = valor / maiorNota.valorNota; 
-				caixa.put(maiorNota, caixa.get(maiorNota) - qtdNotas);
-				notas.put(maiorNota, notas.get(maiorNota) + qtdNotas);
-				valor %= maiorNota.valorNota;
-			    
-				if (caixa.get(maiorNota) < 0) {
-					notas.put(maiorNota, notas.get(maiorNota) + caixa.get(maiorNota));
-					valor += maiorNota.valorNota * (caixa.get(maiorNota) * -1);
-					caixa.put(maiorNota, 0);					
+			qtdCedula = 0;
+			if (valor >= maiorCedula.valorCedula ) {
+				qtdCedula = valor / maiorCedula.valorCedula; 
+				valor %= maiorCedula.valorCedula;
+				
+				caixa.put(maiorCedula, caixa.get(maiorCedula) - qtdCedula);
+				cedulasSacadas.put(maiorCedula, cedulasSacadas.get(maiorCedula) + qtdCedula);
+								
+				if (caixa.get(maiorCedula) < 0) {
+					cedulasSacadas.put(maiorCedula, cedulasSacadas.get(maiorCedula) + caixa.get(maiorCedula));
+					valor += maiorCedula.valorCedula * (caixa.get(maiorCedula) * -1);
+					caixa.put(maiorCedula, 0);					
 				}
 				
 				if (valor > 0) {
-					obterNotas(valor);
+					obterCedulas(valor);
 					return;
 				} 
 			}  
@@ -82,25 +84,28 @@ public class CaixaEletronico {
 		 
 	}
 	
-	private int obterMaiorNotaDisponivel(int valor) {
+	 
+	
+	private int obterCedulaDisponivel(int valor) {
 		
-		if (temNota(Notas.CEM) && valor >= 100 ) {
+		if (temCedula(Cedula.CEM) && valor >= 100 ) {
 			return 100;
 		}
-		if (temNota(Notas.CINQUENTA) && valor >= 50 ) {
-			return 50;
+		if (temCedula(Cedula.CINQUENTA) && valor >= 50 ) {			
+			return 50;		
 		}
-		if (temNota(Notas.VINTE) && valor >= 20 ) {
+		if (temCedula(Cedula.VINTE) && valor >= 20 ) {
 			return 20;
 		}
-		if (temNota(Notas.DEZ) && valor >= 10 ) {
+		if (temCedula(Cedula.DEZ) && valor >= 10 ) {
 			return 10;
 		}
-		if (temNota(Notas.CINCO) && valor >= 5) {
-			if (valor % 5 == 0 || ((valor % 5) % 2 == 0) )
-			return 5;
+		if (temCedula(Cedula.CINCO) && valor >= 5) {
+			if (valor % 5 == 0 || ((valor % 5) % 2 == 0) ) {
+				return 5;
+			}				
 		}
-		if (temNota(Notas.DOIS) && valor >= 2 ) {
+		if (temCedula(Cedula.DOIS) && valor >= 2 ) {
 			return 2;
 		}
 		
@@ -131,8 +136,8 @@ public class CaixaEletronico {
 	
 	private boolean ehMultiplo(int valor) {
 		int resto = 0;
-		for (Map.Entry<Notas, Integer> pair: caixa.entrySet()) {
-			resto = valor % pair.getKey().valorNota;
+		for (Map.Entry<Cedula, Integer> pair: caixa.entrySet()) {
+			resto = valor % pair.getKey().valorCedula;
 			if (resto == 0 || resto == 2 || resto == 5) {
 				return true;
 			}
@@ -140,41 +145,43 @@ public class CaixaEletronico {
 		return false;
 	}
 
-	public void visualizarCaixa() { 
+	public void visualizarCaixa() {
+		System.out.println("##### Cédulas em caixa #####");
 		caixa.forEach((k, v) -> {
-            System.out.format("Notas: %s, Quantidade: %d%n", k, v);
+            System.out.format("Cédula: %s, Quantidade: %d%n", k, v);
         });
+		System.out.println("##### fim caixa #####");
 	}
 	
-	public void visualizarNotasSacadas() {
-		notas.forEach((k, v) -> {
+	public void visualizarCedulasSacadas() {
+		cedulasSacadas.forEach((k, v) -> {
 			if (v > 0) {
-	            System.out.format("%s de %d%n", v, k.valorNota);
+	            System.out.format("%s cédula de %d%n", v, k.valorCedula);
 			}
         });
 	}
 	
-	private boolean temNota(Notas nota) {
+	private boolean temCedula(Cedula nota) {
 		return caixa.get(nota) > 0;
 	}
 	
-	public enum Notas {
+	public enum Cedula {
 	    DOIS(2),CINCO(5),DEZ(10),VINTE(20),CINQUENTA(50),CEM(100);
 	 
-	    public int valorNota;
-	    Notas(int valor) {
-	        valorNota = valor;
+	    public int valorCedula;
+	    Cedula(int valor) {
+	        valorCedula = valor;
 	    }
 	    
-	    public static Optional<Notas> valueOf(int value) {
+	    public static Optional<Cedula> valueOf(int value) {
 	        return Arrays.stream(values())
-	            .filter(n -> n.valorNota == value)
+	            .filter(n -> n.valorCedula == value)
 	            .findFirst();
 	    }
 	    
-	    public static Notas getNota(int index) {
-	        for (Notas n : Notas.values()) {
-	            if (n.valorNota == index) return n;
+	    public static Cedula getCedula(int index) {
+	        for (Cedula n : Cedula.values()) {
+	            if (n.valorCedula == index) return n;
 	        }
 	        throw new IllegalArgumentException("Nota não existe!");
 	     }
@@ -184,8 +191,8 @@ public class CaixaEletronico {
 	public int valorCaixa() {
 		//return caixa.values().stream().mapToInt(Integer::intValue).sum();
 		int valor = 0;
-		for (Map.Entry<Notas, Integer> pair: caixa.entrySet()) {
-			valor += pair.getKey().valorNota * pair.getValue();
+		for (Map.Entry<Cedula, Integer> pair: caixa.entrySet()) {
+			valor += pair.getKey().valorCedula * pair.getValue();
 		}
 		 return valor;
 	}
--- src/com/ciandt/training/challenge1/Main.java
+++ src/com/ciandt/training/challenge1/Main.java
@@ -10,38 +10,9 @@ public class Main {
 			caixaEletronico.visualizarTotalCaixa();
 			System.out.println("Informe o valor do saque: ");
 			
-			caixaEletronico.sacar(2);
+			caixaEletronico.sacar(110);
 			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(4);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(5);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(6);
-			caixaEletronico.visualizarCaixa();			
-			caixaEletronico.sacar(7);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(8);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(9);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(10);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(12);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(17);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(57);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(698);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(9998);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(2222);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(4465);
-			caixaEletronico.visualizarCaixa();
-			caixaEletronico.sacar(1317);
-			caixaEletronico.visualizarCaixa();
+			 
 		} 
 		catch (IllegalArgumentException e) {
 			System.out.println(e.getMessage());
